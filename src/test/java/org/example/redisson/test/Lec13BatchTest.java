ed6ccfaf4b6f1538fffdb34d659de6bbc4e79aeb
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-07-03 11:01:16 +0200

    - BatchTest and RegularTest to save an enormous data to list and set

---
+++ src/test/java/org/example/redisson/test/Lec13BatchTest.java
@@ -0,0 +1,45 @@
+package org.example.redisson.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.BatchOptions;
+import org.redisson.api.RBatchReactive;
+import org.redisson.api.RListReactive;
+import org.redisson.api.RSetReactive;
+import org.redisson.client.codec.LongCodec;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec13BatchTest extends BaseTest{
+
+    //batchTest to save a enormous data in a list or set
+    @Test
+    public void batchTest(){
+
+        RBatchReactive batch = this.client.createBatch(BatchOptions.defaults());
+        RListReactive<Long> list = batch.getList("numbers-list", LongCodec.INSTANCE);
+        RSetReactive<Long> set = batch.getSet("numbers-set", LongCodec.INSTANCE);
+
+        for (long i = 0; i <200_000 ; i++) {
+            list.add(i);
+            set.add(i);
+        }
+        StepVerifier.create(batch.execute().then())
+                .verifyComplete();
+    }
+
+    //regular way to save a enormous data in a list or set
+    @Test
+    public void regularTest(){
+
+        RListReactive<Long> list = this.client.getList("numbers-list", LongCodec.INSTANCE);
+        RSetReactive<Long> set = this.client.getSet("numbers-set", LongCodec.INSTANCE);
+
+        Mono<Void> mono = Flux.range(1, 500_000)
+                .map(Long::valueOf)
+                .flatMap(i -> list.add(i).then(set.add(i)))
+                .then();
+        StepVerifier.create(mono)
+                .verifyComplete();
+    }
+}
